9c4852aaf9a2739e3eb33f9cc5effce0c7f9b831
elaljo <moelalj@c1r3p10.1337.ma>
2024-09-11 10:48:48 +0100

    create folders

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,15 +6,14 @@
 /*   By: moelalj <moelalj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 09:47:04 by moelalj           #+#    #+#             */
-/*   Updated: 2024/09/11 10:06:59 by moelalj          ###   ########.fr       */
+/*   Updated: 2024/09/11 10:38:56 by moelalj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat(){
-	this->name = "Default";
-	this->grade = 75;
+Bureaucrat::Bureaucrat() : name("Default"), grade(75){
+	std::cout << "Called constructor" << std::endl;
 }
 
 Bureaucrat::Bureaucrat(std::string name, int grade){
