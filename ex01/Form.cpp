a25ec00150e8b93218d5ced20aaaff77aadd1e12
Mohamed Elalj <moelalj@c1r1p2.1337.ma>
2024-11-20 20:54:02 +0100

    Form moddified

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: moelalj <moelalj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 16:37:46 by moelalj           #+#    #+#             */
-/*   Updated: 2024/11/18 14:14:52 by moelalj          ###   ########.fr       */
+/*   Updated: 2024/11/20 17:42:40 by moelalj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,23 @@
 Form::Form():name("noname"),grade_to_sign(10),grade_to_exec(20)
 {
     Signed_status = 0;
+    std::cout << "Default Constructor Called" << std::endl;
     if (grade_to_sign < 1 || grade_to_exec < 1)
         throw(Form::GradeTooHighException());
     else if (grade_to_sign > 150 || grade_to_exec > 150)
         throw(Form::GradeTooLowException());
-    std::cout << "Default Constructor Called" << std::endl;
 }
 Form::Form(std::string name, int sign_grade, int sign_exec) : name(name), grade_to_sign(sign_grade), grade_to_exec(sign_exec){
     Signed_status = 0;
+    std::cout << "Constructor Called" << std::endl;
     if (grade_to_sign < 1 || grade_to_exec < 1)
         throw(Form::GradeTooHighException());
     else if (grade_to_sign > 150 || grade_to_exec > 150)
         throw(Form::GradeTooLowException());
-    std::cout << "Constructor Caleld" << std::endl;
 }
 Form::Form(const Form& rhs):name(rhs.name),grade_to_sign(rhs.grade_to_sign),grade_to_exec(rhs.grade_to_exec){
-    Signed_status = rhs.Signed_status;
     std::cout << "Copy Constructor Called" << std::endl;
+    Signed_status = rhs.Signed_status;
 }
 Form& Form::operator=(const Form& rhs){
     std::cout << "Copy Assignment Called" << std::endl;
