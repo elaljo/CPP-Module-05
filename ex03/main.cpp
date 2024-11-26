72e773926623922022510a3b5b391018b86957f7
Mohamed Elalj <moelalj@c1r1p2.1337.ma>
2024-11-26 16:24:38 +0100

    done

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: moelalj <moelalj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 14:51:58 by moelalj           #+#    #+#             */
-/*   Updated: 2024/11/20 20:52:06 by moelalj          ###   ########.fr       */
+/*   Updated: 2024/11/26 16:21:50 by moelalj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int main(){
     Intern someRandomIntern;
     AForm* rrf;
     
-    rrf = someRandomIntern.makeForm("presidential pardon", "Bender");
+    rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+    
+    delete rrf;
     return 0;
 }
